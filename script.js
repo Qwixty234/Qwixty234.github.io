d966bb17fb7ec770ba34ec5e226f0913aad282e8
Qwixty234 <yannet.aragonvega123@gmail.com>
2026-03-13 23:01:30 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+    <script language="javascript">
+    var titletext="L o v e l i 3 w s ! !"
+    var repeat=true;
+    set=setTimeout;
+    var index=0;
+    function scrolltitle(){
+    if(index<=titletext.length){
+    document.title=titletext.substring(0,index);
+    index++;
+    set('scrolltitle()', 200);
+    }else{
+    index=0;
+    if(repeat)set('scrolltitle()',1000);
+    }}
+    window.onload=function(){
+    if(!document.layers)set('scrolltitle()',1000);
+    }
